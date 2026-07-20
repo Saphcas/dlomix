a94ea1a79d047c0a5299bb5dd7d3b2bfc18fda57
Cas <cassandra.wivegg@gmail.com>
2026-07-20 16:16:48 +0200

    Forgot to add the removal part of the str conversion

--- run_scripts/train_prosit_intensity_ptms_torch.py
+++ run_scripts/train_prosit_intensity_ptms_torch.py
@@ -502,7 +502,9 @@ def _mae_from_log_mean(y_true: torch.Tensor, pred_log_mean: torch.Tensor) -> flo
     pred_intensity = _log_mean_to_intensity(pred_log_mean)
     target = _target_intensity_for_metrics(y_true)
     # Testing if torch conversion is causing peaks in mae. Instead of using item turn float output to string
-    mean_string_to_float = eval(str(torch.mean(torch.abs(pred_intensity - target)).float()))
+    string_number = str(torch.mean(torch.abs(pred_intensity - target)).float())
+    string_number = str(string_number)[:-1].replace("tensor(", "")
+    mean_string_to_float = eval(string_number)
     return mean_string_to_float #torch.mean(torch.abs(pred_intensity - target)).float().item()
 
 
