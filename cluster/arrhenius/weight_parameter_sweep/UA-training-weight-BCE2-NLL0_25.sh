a38b002b07ebaadd47e5137d4f3c4d1f4baaa58b
Cas <cassandra.wivegg@gmail.com>
2026-07-28 16:22:51 +0200

    Modified weighting scripts to use the correct container

--- cluster/arrhenius/weight_parameter_sweep/UA-training-weight-BCE2-NLL0_25.sh
+++ cluster/arrhenius/weight_parameter_sweep/UA-training-weight-BCE2-NLL0_25.sh
@@ -54,7 +54,7 @@ trap on_term TERM
 trap 'echo "[signal] caught SIGINT"; exit 130' INT
 
 apptainer exec --bind $TMPDIR/ \
-    /nobackup/proj/disk/kall/personal/$USER/containers/dlomix-ngc-26.06.sif \
+    /nobackup/proj/disk/kall/personal/$USER/containers/dlomix-weights.sif \
     python /opt/dlomix-src/run_scripts/train_prosit_intensity_ptms_torch.py
 
 # scp -r $CHECKPOINT_DIR /nobackup/proj/disk/kall/personal/$USER/checkpoints/
